910a029b3138c0f2f3b338cbd45be505f7e6202e
unknown <den200710@yandex.ru>
2026-02-01 19:51:47 +0300

    Page Factory

--- components/authentication/login_form_component.py
+++ components/authentication/login_form_component.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import Page, expect
 from components.base_component import BaseComponent
+from elements.input import Input
 
 
 class LoginFormComponent(BaseComponent):
@@ -7,13 +8,13 @@ class LoginFormComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.email_input = page.locator('//div[@data-testid="login-form-email-input"]//div//input')
-        self.password_input = page.get_by_test_id('login-form-password-input').locator('input')
+        self.email_input = Input(page, 'login-form-email-input', 'Email')
+        self.password_input = Input(page, 'login-form-password-input', 'Password')
 
     def fill(self, email: str, password: str):
         self.email_input.fill(email)
         self.password_input.fill(password)
 
     def check_visible(self, email: str, password: str):
-        expect(self.email_input).to_have_value(email)
-        expect(self.password_input).to_have_value(password)
+        self.email_input.check_have_value(email)
+        self.password_input.check_have_value(password)
